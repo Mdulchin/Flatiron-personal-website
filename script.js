f6434715a357df92f2e16d1491478256dd2c60bb
madelinedulchin <madeline.dulchin@gmail.com>
2022-09-12 14:09:31 -0400

    adding text when hovering over image

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
+h2.textContent = "My Personal Website";
 document.querySelector("body").appendChild(h2);
+
+var e = document.getElementById('me');
+
+e.onmouseover = function() {
+  document.getElementById('popup').style.display = 'block';
+}
+
+e.onmouseout = function() {
+  document.getElementById('popup').style.display = 'none';
+}   
+
+
+
